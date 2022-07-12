13385cbf5970b5484aebbeee9757987e55587f62
sigltech <101203107+sigltech@users.noreply.github.com>
2022-07-12 16:17:45 +0100

    setting up Router and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { IndexPage, SearchPage, UserDataPage, RepoPage } from './pages';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes path='/' >
+
+        <Route path='/' element={<IndexPage />} />
+        <Route path='search'>
+          <Route path='/search' element={<SearchPage />} />
+          <Route path=':UserDataPage' element={<UserDataPage />} >
+            <Route path=':repo' element={<RepoPage />} />
+          </Route>
+        </Route>
+
+      </Routes>
     </div>
   );
 }
