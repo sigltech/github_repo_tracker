6a59f69013d0d711d0afc25c33cc60c145c7bcdb
mariesophiecd <mariesophie.cduran@gmail.com>
2022-07-12 18:02:53 +0100

    added API function to SearchForm

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 
 function SearchForm(){
     const [username, setUsername] = useState('')
+    const [submitValue, setSubmitValue] = useState('mariesophiecd')
 
     function handleUsername(e){
         setUsername(e.target.value);
@@ -9,14 +10,35 @@ function SearchForm(){
 
     function handleSubmit(e){
         e.preventDefault()
-        setUsername()
+        setSubmitValue(username)
+        setUsername('')
     }
+
+    useEffect(() => {
+        
+        async function fetchRepos(username){
+            try{
+                const result = await fetch(`https://api.github.com/users/${username}/repos`);
+                console.log(result)
+                const data = await result.json();
+                console.log(data)
+
+            }catch(err){
+                console.log(err)
+            }
+            
+           
+        }
+        fetchRepos()
+    }, [])
+
+
 
     return(
         <>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="enter_username">Type a username</label>
-                <input type="text" name="enter_username" OnChange={handleUsername} value={username}/>
+                <input type="text" name="enter_username" onChange={handleUsername} value={username}/>
                 <input type="submit" value="Send!" />
             </form>
         </>
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SearchForm from "../../components/SearchForm";
+// import { UserContext } from "../../context/RepoContext";
 
 export default function SearchPage() {
+    // const [users, setUsers] = useContext(UserContext)
+
     return (
-        <h1>This is the SearchPage</h1>
+        <>
+            <h1>This is the SearchPage</h1>
+            <SearchForm />
+        </>
     );
 }
