6be69f4ec6b4aa22aae883344cdee9619c52d273
sigltech <101203107+sigltech@users.noreply.github.com>
2022-07-12 17:06:59 +0100

    styled indexPage and created navigation from index to search page

--- src/pages/IndexPage/index.jsx
+++ src/pages/IndexPage/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import './style.css'
+import { useNavigate } from "react-router-dom";
 
 export default function IndexPage() {
+  const navigate = useNavigate();
+
+  function navigateToSearch() {
+    navigate("/search");
+  }
+
   return (
-    <>
+    <div className="index-container">
+
       <h1>Learn about a Github Users information</h1>
-      <button>Press to begin Search</button>
-    </>
+      <button onClick={navigateToSearch}>Press to begin Search</button>
+
+    </div>
     )
 }
