671374909e235606a043f35105d203f558f7f578
mariesophiecd <mariesophie.cduran@gmail.com>
2022-07-13 16:43:01 +0100

    added path to API in RepoPage

--- src/pages/RepoPage/index.jsx
+++ src/pages/RepoPage/index.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState} from "react";
 import axios from "axios";
 import { RepoCard } from "../../components/RepoCard";
 import { useParams } from "react-router-dom";
+import { SearchForm } from '../../components/SearchForm';
+import { RepoContext } from "../../context/RepoContext";
 
 export default function RepoPage() {
     const {name} = useParams();
-    const [repo, setRepo] = useState({});
+    const [repo, setRepo] = useContext(RepoContext);
 
     useEffect( () => {
 
         async function fetchRepo(name) {
 
-            const GITHUB_USER_URL = `https://api.github.com/users/${name}/repos`;
+            const GITHUB_USER_URL = `https://api.github.com/repos/${username}/${name}`;
             const result = await axios.get(GITHUB_USER_URL)
             setRepo(result.data);
             
